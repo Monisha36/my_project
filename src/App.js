e37130fb8a52be9f8966ed740c819950f18a94bb
Monisha Subramani <v-monishas@microsoft.com>
2023-07-05 11:38:02 +0530

    Search Feild added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+ 
 import './App.css';
 
 class App extends Component {
@@ -8,25 +8,50 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName:'Monisha', lastName:'Monster'},
-      company: 'VueData',
+      movies: [],
+      searchField: ''
     };
+    console.log('constructor');
   }
 
-  render(){
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+       .then((response) => response.json())
+       .then((users) => 
+       this.setState(
+       () => {
+        return {movies: users}
+       },
+       () => {
+        console.log (this.state);
+       }
+       ))
+  }
+  render() {
+    console.log('render');
+    const filteredMovies = this.state.movies.filter((movie) => {
+      return movie.name.toLocaleLowerCase().includes(this.state.searchField);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {this.state.name.firstName} { this.state.name.lastName}, I work at {this.state.company}
-        </p>
-        <button 
-        onClick={() => {
-        this.setState({ name: {firstName:'Andria' , lastName:'Monster'},})
-        }}>
-           Change Name</button>
-      </header>
+      <input className= 'serach-box' type='search' placeholder='Serach Movies'
+      onChange={(event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(() => {
+          return { searchField };
+        }
+        );
+      }}
+      />
+      {
+        filteredMovies.map((movie) => {
+          return <div key={movie.id}>
+            <h1>{movie.name}</h1>
+            </div>
+        })
+      }
     </div>
   );
   }
